b451f6f965e557f171937f25ea736af688dddb50
Jing Zhang <jiz346@g.harvard.edu>
2021-04-16 00:03:42 -0700

    init and scaffolding

---
+++ app/src/main/java/com/example/explorealaska/CategoryAdapter.java
@@ -0,0 +1,79 @@
+package com.example.explorealaska;
+
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+/**
+ * {@link CategoryAdapter} is a {@link FragmentPagerAdapter} that can provide the layout for
+ * each list item based on a data source which is a list of {@link Word} objects.
+ */
+public class CategoryAdapter extends FragmentPagerAdapter {
+
+    private final Context mContext;
+
+    /**
+     * Create a new {@link CategoryAdapter} object.
+     *
+     * @param context                is the context of the app
+     * @param supportFragmentManager is the fragment manager that will keep each fragment's state in the adapter across swipes.
+     */
+    public CategoryAdapter(Context context, FragmentManager supportFragmentManager) {
+        super(supportFragmentManager);
+        mContext = context;
+    }
+
+    /**
+     * Return the Fragment associated with a specified position.
+     */
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        switch (position) {
+            case 0:
+                return new WildlifeFragment();
+            case 1:
+                return new ExperienceFragment();
+            case 2:
+                return new ParkFragment();
+            default:
+                return new ParkFragment();
+        }
+    }
+
+    /**
+     * Return the number of views available.
+     */
+    @Override
+    public int getCount() {
+        return 4;
+    }
+
+    /**
+     * This method may be called by the ViewPager to obtain a title string
+     * to describe the specified page. This method may return null
+     * indicating no title for this page. The default implementation returns
+     * null.
+     *
+     * @param position The position of the title requested
+     * @return A title for the requested page
+     */
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        switch (position) {
+            case 0:
+                return mContext.getString(R.string.category_wildlife);
+            case 1:
+                return mContext.getString(R.string.category_experience);
+            case 2:
+                return mContext.getString(R.string.category_park);
+            default:
+                return mContext.getString(R.string.category_park);
+        }
+    }
+}
---
+++ app/src/main/java/com/example/explorealaska/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.explorealaska;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Set the content of the activity to use the activity_main.xml layout file
+        setContentView(R.layout.activity_main);
+
+        // Find the view pager that will allow the user to swipe between fragments
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+
+        // Create an adapter that knows which fragment should be shown on each page
+        CategoryAdapter adapter = new CategoryAdapter(this, getSupportFragmentManager());
+
+
+    }
+}
